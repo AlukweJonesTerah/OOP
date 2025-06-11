a9ccf1403548750143a0a6ad1bfc675b31edee30
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2025-06-11 04:45:48 +0300

    classes

--- dist/index.js
+++ dist/index.js
@@ -1,62 +1,11 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
-let sales = 123456;
-let course = "IT";
-let is_pblished = true;
-function render(document) {
-    console.log(document);
-}
-let numbers = [1, 2, 3];
-let numbers_ = [];
-let user = [1, 'Jones'];
-let user_ = [1, "Alex", true, 0];
-const Small = 1;
-const Middle = 2;
-const Large = 3;
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 0] = "Small";
-    Size[Size["Middel"] = 1] = "Middel";
-    Size[Size["Large"] = 2] = "Large";
-})(Size || (Size = {}));
-var Size_;
-(function (Size_) {
-    Size_["Small_"] = "s";
-    Size_["Middle_"] = "m";
-    Size_["Large_"] = "l";
-})(Size_ || (Size_ = {}));
-let mySize = Size_.Middle_;
-console.log(mySize);
-function calculationTax(income, taxYear) {
-    if (taxYear < 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
-calculationTax(101000, 2022);
-function calculationTax_(income, taxYear) {
-    if ((taxYear || 2024) < 2025)
-        return income * 1.2;
-    return income * 1.3;
-}
-calculationTax_(20200);
-function calculationTax__(income, taxYear = 2022) {
-    if (taxYear < 2024)
-        return income * 1.2;
-    return income * 1.3;
-}
-calculationTax__(30300);
-let employee = { id: 13 };
-let employee_ = { id: 1, name: '' };
-employee_.name = "Jones";
-let employee__ = { id: 1, name: "Alex" };
-let employee___ = {
-    id: 1,
-    name: "Alukwe",
-    retire: (date) => {
-        console.log(date);
+console.log("TSC is here");
+console.log("we are still here");
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
     }
-};
+}
+const jones = new User("j@j.com", "jones");
 //# sourceMappingURL=index.js.map
---
+++ dist/index_.js
@@ -0,0 +1,93 @@
+"use strict";
+let age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+let sales = 123456;
+let course = "IT";
+let is_pblished = true;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+let numbers_ = [];
+let user = [1, 'Jones'];
+let user_ = [1, "Alex", true, 0];
+const Small = 1;
+const Middle = 2;
+const Large = 3;
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 0] = "Small";
+    Size[Size["Middel"] = 1] = "Middel";
+    Size[Size["Large"] = 2] = "Large";
+})(Size || (Size = {}));
+var Size_;
+(function (Size_) {
+    Size_["Small_"] = "s";
+    Size_["Middle_"] = "m";
+    Size_["Large_"] = "l";
+})(Size_ || (Size_ = {}));
+let mySize = Size_.Middle_;
+console.log(mySize);
+function calculationTax(income, taxYear) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculationTax(101000, 2022);
+function calculationTax_(income, taxYear) {
+    if ((taxYear || 2024) < 2025)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculationTax_(20200);
+function calculationTax__(income, taxYear = 2022) {
+    if (taxYear < 2024)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculationTax__(30300);
+let employee = { id: 13 };
+let employee_ = { id: 1, name: '' };
+employee_.name = "Jones";
+let employee__ = { id: 1, name: "Alex" };
+let employee___ = {
+    id: 1,
+    name: "Alukwe",
+    retire: (date) => {
+        console.log(date);
+    }
+};
+let employee____ = {
+    id: 1,
+    name: "Alukwe",
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+let weight;
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 50;
+let quantity_ = 100;
+let quantity__ = 100;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola');
+}
+greet(null);
+let newDate = () => {
+    return 0;
+};
+//# sourceMappingURL=index_.js.map
